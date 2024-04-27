fbc2278b3a35a7658213f98aacae423c82b4d1df
Júlia D. Craide <juliadcraide@hotmail.com>
2024-04-27 00:39:50 -0300

    Forklift push with object that touch trigger in the palett

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -6,6 +6,7 @@ public class Rotate : MonoBehaviour {
 
     public Character c;
     public float rotationSpeed = 150;
+    public float pushPower = 5.0f;
 
     // Start is called before the first frame update
     void Start() {
@@ -18,4 +19,31 @@ public class Rotate : MonoBehaviour {
         transform.Rotate(horizontalRotation);
         c.forward = transform.forward;
     }
+
+    private void OnTriggerEnter(Collider other) {
+        Rigidbody body = other.attachedRigidbody;
+
+        Debug.Log("Colision start");
+
+        if (body == null || body.isKinematic) {
+            return;
+        }
+        //if (hit.moveDirection.y < -0.3) {
+        //    return;
+        //}
+        Debug.Log("Colision");
+
+        //Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        // If you know how fast your character is trying to move,
+        // then you can also multiply the push velocity by that.
+
+        // Apply the push
+        body.velocity = /*pushDir*/ c.forward * pushPower;
+        Vector3 pushDir = (body.transform.position - this.transform.position).normalized;
+
+        body.velocity = pushDir * pushPower * Input.GetAxis("Vertical");
+
+    }
+
 }
