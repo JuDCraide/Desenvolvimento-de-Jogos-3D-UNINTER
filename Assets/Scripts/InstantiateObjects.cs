88093ce45e6a83a62339c98b143e05dc643791ce
Júlia D. Craide <juliadcraide@hotmail.com>
2024-07-16 20:27:51 -0300

    Level Design

--- Assets/Scripts/InstantiateObjects.cs
+++ Assets/Scripts/InstantiateObjects.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 [Serializable]
 public class ObjectQuantity {
-    public GameObject pickableObject;
+    public GameObject go;
     public int quantity;
 }
 
@@ -17,11 +17,11 @@ public class InstantiateObjects : MonoBehaviour {
         foreach (var obj in objects) {
             for (int i = 0; i < obj.quantity; i++) {
                 Vector2 pos2d = UnityEngine.Random.insideUnitCircle * 100;
-                Vector3 pos3d = new Vector3(pos2d.x - 10, 0.01f, pos2d.y - 10);
+                Vector3 pos3d = new Vector3(pos2d.x - 10, 0.1f, pos2d.y - 10);
                 //Instantiate(obj.pickableObject, this.transform + pos3d, Quaternion.identity);
-                var go = Instantiate(obj.pickableObject, this.transform);
+                var go = Instantiate(obj.go, this.transform);
                 go.transform.position = pos3d;
             }
         }
     }
-}
+}   
